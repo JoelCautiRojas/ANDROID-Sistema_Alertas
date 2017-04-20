7736e458977538b6ae2878e76b40d55a28849f9e
Joel Cauti Rojas <joel.cauti@gmail.com>
2017-04-20 13:29:54 -0500

    Primer Commit
    Primer Commit

---
+++ app/src/main/java/com/example/joel_64/apppnp/PreloadActivity.java
@@ -0,0 +1,13 @@
+package com.example.joel_64.apppnp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PreloadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preload);
+    }
+}
